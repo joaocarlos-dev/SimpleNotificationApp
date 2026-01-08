b87422f3276c6a26b7726192cab84e68889a455b
joao.carlos <joao.carlos@sebsa.com.br>
2026-01-08 16:07:16 -0300

    feat: add Enum and Notification entity

---
+++ SimpleNotificationApp.Domain/Entities/Notification.cs
@@ -0,0 +1,34 @@
+﻿using SimpleNotificationApp.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleNotificationApp.Domain.Entities
+{
+    public class Notification
+    {
+        public NotificationType Type { get; }
+        public string Message { get; }
+        public string Recipient { get; }
+
+        public Notification(
+            NotificationType type, 
+            string message, 
+            string recipient)
+        {
+            if (string.IsNullOrEmpty(message))
+            {
+                throw new ArgumentNullException("Message can not be Null or empty");
+            }
+            if (string.IsNullOrEmpty(recipient))
+            {
+                throw new ArgumentNullException("Recipient can not be Null or empty");
+            }
+            Type = type;
+            Message = message;
+            Recipient = recipient;
+        }
+    }
+}
---
+++ SimpleNotificationApp.Domain/Enums/NotificationType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleNotificationApp.Domain.Enums
+{
+    public enum NotificationType
+    {
+        Wpp, 
+        sms, 
+        email
+    }
+}
