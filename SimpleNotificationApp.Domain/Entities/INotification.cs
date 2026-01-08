37fe72280cf2a376dc0f40b7a13cb9f39b505077
joao.carlos <joao.carlos@sebsa.com.br>
2026-01-08 15:07:56 -0300

    added folders onion architecture structure

---
+++ SimpleNotificationApp.Domain/Entities/INotification.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleNotificationApp.Domain.Entities
+{
+    public class Notification
+    {
+        public string Type { get; }
+        public string Message { get; }
+        public string Recipient { get; }
+
+    }
+}
